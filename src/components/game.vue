f04acbc66243d9f7cc2abb2b2e02d9eba4689dac
richie-qin <773726433@qq.com>
2019-12-29 21:04:15 +0800

    修改

--- src/components/game.vue
+++ src/components/game.vue
@@ -16,12 +16,19 @@
             <img src="../assets/image/img10.png" alt="">
             <img src="../assets/image/img11.png" alt="">
         </div>
-
+        <div id="userHead">
+            <div id="head"><img src="../assets/image/img13.png" alt=""></div>
+            <div id="userId">248797</div>
+        </div>
+        <div id="userGrade" :style="{top:gradeTop+'px'}">当前等级:LV2</div>
+        <div id="userIntegral" :style="{top:integralTop+'px'}"><img src="../assets/image/img14.png" alt=""><span>{{intIntegral}}</span></div>
         <div id="topImage">
             <img src="../assets/image/img12.png" alt="" @load="topImgLoad">
         </div>
         <div id="box" v-show="showBox">
-            <div v-for="(item,index) in 9" :key="index" class="pos" :id="'pos'+(index+1)"></div>
+            <div v-for="(item,index) in 9" :key="index" class="pos" :id="'pos'+(index+1)">
+                <div id="ProfitDom"  :class="listState[index].show?'ProfitAnima':''"><img src="../assets/image/img14.png" alt=""><span>+{{currentNum[index]}}</span></div>
+            </div>
      
             <div v-for="(item,index) in 9" :key="index" v-show="listState[index].show" class="pos-span" :id="'span'+(index+1)" :style="{left:listState[index].grade==1?coordinate[index].left+'px':coordinate[index].left-12+'px',top:coordinate[index].top+'px'}" @touchstart="touchstart(index+1)" @touchmove="touchmove(index+1)" @touchend="touchend(index+1)"><img :src="listState[index].grade==1?require('../assets/image/img06.png'):require('../assets/image/img05.png')" alt=""></div>
 
@@ -35,6 +42,29 @@
                 <img src="../assets/image/img09.png" alt="">
             </div>
         </div>
+        <!-- 中奖了 -->
+        <div id="winning" v-show="showWin">
+            <img src="../assets/image/img06.png" alt="" id="popupHB">
+            <div id="centerBox">
+                <img src="../assets/image/img02.png" alt="" id="popupImg1">
+                <img src="../assets/image/img03.png" alt="" id="popupImg2" @click="closeP(1)">
+                <img src="../assets/image/img01.png" alt="" id="popupImg3">
+            </div>
+        </div>
+
+        <!-- 升级了 -->
+        <div id="upgraded" v-show="showUpgraded">
+            <img src="../assets/image/img06.png" alt="" id="upgradedHB">
+            <div id="centerBox2">
+                <img src="../assets/image/img10.png" alt="" id="popupImg4">
+                <img src="../assets/image/img11.png" alt="" id="popupImg5">
+                <img src="../assets/image/img03.png" alt="" id="popupImg6" @click="closeP(2)">
+
+            </div>
+        </div>
+
+        <!-- 积分不足 -->
+        <span id="noIntegral" v-show="noIntegral">人气不足，请稍等~</span>
     </div>
 </template>
 
@@ -57,6 +87,7 @@ export default {
             pos7:null,
             pos8:null,
             pos9:null,
+            chaichu:null,
 
             dfdistanceTop1:0,
             dfdistanceTop2:0,
@@ -69,7 +100,9 @@ export default {
             t3:0,l3:0,r3:0,b3:0,t4:0,l4:0,r4:0,b4:0,
             t5:0,l5:0,r5:0,b5:0,t6:0,l6:0,r6:0,b6:0,
             t7:0,l7:0,r7:0,b7:0,t8:0,l8:0,r8:0,b8:0,
-            t9:0,l9:0,r9:0,b9:0,
+            t9:0,l9:0,r9:0,b9:0,ccT:0,ccL:0,ccR:0,ccB:0,
+
+
 
             dfX1:0,dfY1:0,
             dfX2:0,dfY2:0,
@@ -98,6 +131,27 @@ export default {
             showBox:false,
             hongbaoW:0,
             hongbaoH:0,
+            showWin:false,
+            showUpgraded:false,
+            noIntegral:false,
+            gradeTop:0,
+            integralTop:0,
+            intGrade:1,//当前等级
+            intIntegral:7000,//当前积分
+            currentNum:[171,135,198,225,315,156,119,275,310],
+            numArr:[100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575],
+            onesGoHome:0,
+            setInterval1:null,
+            setInterval2:null,
+            setInterval3:null,
+            setInterval4:null,
+            setInterval5:null,
+            setInterval6:null,
+            setInterval7:null,
+            setInterval8:null,
+            setInterval9:null,
+            noIntegralTime:null,
+
         }
     },
 
@@ -107,12 +161,10 @@ export default {
 
     created(){
         
-        
-
     },
 
     mounted(){
-        
+
     },
 
     computed(){
@@ -131,9 +183,17 @@ export default {
             this.pos7 = $('#span7');
             this.pos8 = $('#span8');
             this.pos9 = $('#span9');
+            this.chaichu = $('#button07');
         },
         topImgLoad(){
             this.tophongbaoHeight = $("#topImage").height();
+            let gradeH = $("#userGrade").height();//等级文字高度
+            let gradeBGH = 0.195*this.tophongbaoHeight;//等级背景高度
+            let integralH = $("#userGrade").height();//积分文字高度
+            let integralBGH = 0.195*this.tophongbaoHeight;//积分背景高度
+
+            this.gradeTop = 0.45*this.tophongbaoHeight+((gradeBGH-gradeH)/2);
+            this.integralTop = 0.61 *this.tophongbaoHeight + ((integralBGH-integralH)/2);
             this.defaultWH();
             this.showBox = true;
         },
@@ -141,7 +201,7 @@ export default {
         defaultWH(){
             this.$nextTick(()=>{
                 this.getDom();
-                let boxH = this.bodyH - this.tophongbaoHeight - 90;
+                let boxH = this.bodyH - this.tophongbaoHeight - 100;
                 let boxW =  this.bodyW;
                 let posH = 0;
                 let posW = 0;
@@ -247,6 +307,12 @@ export default {
                     this.l9 = this.dfdistanceLeft3;
                     this.r9 = this.dfdistanceLeft3 + boxWidth;
                     this.b9 = this.dfdistanceTop3 + boxHeight;
+
+                    this.ccT = this.chaichu.offset().top-this.tophongbaoHeight;//拆除位置
+                    this.ccL = this.chaichu.offset().left;
+                    this.ccR = this.ccL+this.chaichu.width();
+                    this.ccB = this.ccT+this.chaichu.height();
+
                 }
                 
             })
@@ -265,25 +331,8 @@ export default {
             let domx = this[`dfX${this.choiseIndex}`]
             let domy = this[`dfY${this.choiseIndex}`]
 
-
-
             this.coordinate[arryIndex].left = ev.clientX - domx - (this.hongbaoW/2);
             this.coordinate[arryIndex].top = ev.clientY - domy - this.tophongbaoHeight - (this.hongbaoH/2);
-
-            // let newt1 = this.coordinate[arryIndex].top;
-            // let newr1 = this.coordinate[arryIndex].left + 50;
-            // let newb1 = this.coordinate[arryIndex].top + 50;
-            // let newl1 = this.coordinate[arryIndex].left;
-
-            // for(let i=1;i<10;i++){
-            //     if(this.choiseIndex!=i){
-            //         if(newb1>this[`t${i}`] && newl1<this[`r${i}`] && newt1<this[`b${i}`] && newr1>this[`l${i}`]){
-            //             console.log(`第${this.choiseIndex}个碰上了第${i}个`);
-            //         }
-            //     }else{
-
-            //     }
-            // }
 
         },
         touchend(index){
@@ -311,24 +360,36 @@ export default {
             }
 
             if(TouchTF&&TouchIndex>0&&TouchIndex<10){//有触碰，判断等级一致才合并
-                console.log("第"+index+"个"+"碰到了第"+TouchIndex+"个");
+                console.log("第"+this.choiseIndex+"个"+"碰到了第"+TouchIndex+"个");
                 if(this.listState[arryIndex].grade==1&&this.listState[TouchIndex-1].grade==1){
                     this.listState[arryIndex].show = false;
                     this.listState[arryIndex].grade = 0;
                     this.listState[TouchIndex-1].grade = 2;
+                    clearInterval(this[`setInterval${index}`]);
                 }else if(this.listState[arryIndex].grade==2&&this.listState[TouchIndex-1].grade==2){
-                    console.log("此处应该弹出中奖页面");
+                    // console.log("此处应该弹出中奖页面");
                     this.listState[arryIndex].show = false;
-
+                    this.listState[arryIndex].grade = 0;
+                    this.showWin = true;
+                    clearInterval(this[`setInterval${index}`]);
                 }else{
                     this.recoveryDf(index);
                 }
                 this[`pos${index}`].css("transition","all 0.5s");
                 this[`pos${index}`].css("z-index","0");
 
-            }else{//无触碰
-                this.recoveryDf(index);
-                this[`pos${index}`].css("transition","all 0.5s");
+            }else{//无触碰,判断是否移除操作
+                if(newb1>this.ccT&& newl1<this.ccR&& newt1<this.ccB&& newr1>this.ccL){
+                    // console.log("已经移除");
+                    this.listState[arryIndex].show = false;
+                    this.listState[arryIndex].grade = 0;
+                    this[`pos${index}`].css("transition","all 0s");
+                    clearInterval(this[`setInterval${index}`]);
+                }else{
+                    console.log("归位");
+                    this.recoveryDf(index);
+                    this[`pos${index}`].css("transition","all 0.5s");
+                }
                 this[`pos${index}`].css("z-index","0");
             }
         },
@@ -366,20 +427,63 @@ export default {
             }
         },
 
+        setInter(index){
+            let listIndex = index+1;
+            let newArry1 = JSON.parse(JSON.stringify(this.currentNum));
+            let that = this;
+            this[`setInterval${listIndex}`] = setInterval(() => {
+                newArry1[index] = that.randomNum();
+                that.intIntegral+=newArry1[index];
+                that.currentNum = JSON.parse(JSON.stringify(newArry1));
+            }, 9800);
+
+        },
+        setTime(index){
+            let that = this;
+            setTimeout(() => {
+                that.intIntegral+=that.currentNum[index];
+                that.setInter(index);
+            }, 4500);
+            
+        },
         //空位填充
         addItem(){
             let newArry = JSON.parse(JSON.stringify(this.listState));
             for(let i=0;i<newArry.length;i++){
                 if(!newArry[i].show){
-                    this.recoveryDf(i+1);
-                    newArry[i].show = true;
-                    newArry[i].grade = 1;
+                    if(this.intIntegral>=1000){
+                        this.recoveryDf(i+1);
+                        newArry[i].show = true;
+                        newArry[i].grade = 1;
+                        this.intIntegral-=1000;
+                        this.setTime(i);
+                    }else{
+                        console.log("积分不足");
+                        this.noIntegral = true;
+                        clearTimeout(this.noIntegralTime);
+                        this.noIntegralTime = setTimeout(() => {
+                            this.noIntegral = false;
+                        }, 1500);
+                    }
                     
                     break;
                 }
             }
             this.listState = JSON.parse(JSON.stringify(newArry));
+        },
+
+        closeP(index){
+            if(index==1){
+                this.showWin = false;
+
+            }else{
+                this.showUpgraded = false;
+            }
+        },
+        randomNum(minNum=100,maxNum=500){ 
+            return parseInt(Math.random()*(maxNum-minNum)+minNum);
         }
+
     },
 
     filters:{
@@ -387,7 +491,31 @@ export default {
     },
 
     watch:{
-
+        intIntegral(val,old){
+            if(val>=0&&val<4999){
+                this.intGrade = 1;
+            }else if(val>=5000&&val<9999){
+                this.intGrade = 2;
+            }else if(val>=10000&&val<14999){
+                this.intGrade = 3;
+            }else if(val>=15000&&val<19999){
+                this.intGrade = 4;
+            }else if(val>=20000&&val<24999){
+                this.intGrade = 5;
+            }else if(val>=25000&&val<29999){
+                this.intGrade = 6;
+            }else if(val>=30000&&val<34999){
+                this.intGrade = 7;
+            }else if(val>=35000&&val<39999){
+                this.intGrade = 8;
+            }else if(val>=40000&&val<44999){
+                this.intGrade = 9;
+            }else if(val>=45000&&val<49999){
+                this.intGrade = 10;
+            }else if(val>=50000&&val<54999){
+                this.intGrade = 11;
+            }
+        }
     },
 
     //离开路由，不可截停
@@ -410,9 +538,75 @@ export default {
         overflow: hidden;
         position: relative;
     }
+    #userHead{
+        position:absolute;
+        top: 15px;
+        left: 20px;
+        height: 45px;
+        // width: 100%;
+        font-size: 0;
+        #head{
+            width: 45px;
+            height: 45px;
+            border-radius: 45px;
+            background: blue;
+            display: inline-block;
+            vertical-align: middle;
+            z-index: 1;
+            position: relative;
+            border: solid 2px #fff;
+            overflow: hidden;
+        }
+        #userId{
+            width: 80px;
+            height: 25px;
+            border: 30px;
+            background: #acc824;
+            display: inline-block;
+            vertical-align: middle;
+            border-radius: 0 25px 25px 0;
+            margin-left: -10px;
+            line-height: 25px;
+            color: #fff;
+            font-size: 14px;
+            overflow: hidden;
+            border: solid 1.5px #fff;
+        }
+    }
+    #userGrade{
+        position: absolute;
+        // top: 16%;
+        left: calc(12vw + 10px);
+        transform:rotate(-5deg);
+        color: #9e1818;
+        font-size: 3.7vw;
+        font-weight: bold;
+        text-shadow: #fff 1px 0 0,#fff 0 1px 0,#fff -1px 0 0 ,#fff 0 -1px 0;
+    }
+    #userIntegral{
+        position: absolute;
+        top: 23.5%;
+        left: 23px;
+        color: #9e1818;
+        font-weight: bold;
+        // -webkit-text-stroke: 1px #fff;
+        text-shadow: #fff 1px 0 0,#fff 0 1px 0,#fff -1px 0 0 ,#fff 0 -1px 0;
+        transform:rotate(5deg);
+        
+        img{
+            width: 6vw;
+            vertical-align: middle;
+        }
+        span{
+            vertical-align: middle;
+            margin-left: 2px;
+            font-size: 3.7vw;
+        }
+    }
     #topImage{
         width: 100%;
         height: auto;
+        font-size: 0;
         img{
             width: 100%;
             height: auto;
@@ -440,6 +634,7 @@ export default {
             background-size: 75%;
             background-position: 50% 80%;
             font-size: 0;
+            position: relative;
             
         }
         .pos-span{
@@ -462,9 +657,9 @@ export default {
             position: absolute;
             bottom: 0;
             width: 100%;
-            height: 90px;
+            height: 100px;
             #buttonD1{
-                height: 35px;
+                height: 45px;
                 width: 100%;
                 img{
                     height: 100%;
@@ -472,9 +667,12 @@ export default {
                 }
                 #button07{
                     margin-left: 10px;
+                    vertical-align: middle;
                 }
                 #button08{
-                    margin-left: 10%;
+                    margin-left: 18%;
+                    height: 35px;
+                    vertical-align: middle;
                 }
             }
             #buttonD2{
@@ -488,4 +686,158 @@ export default {
             }
             
         }
+#winning{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: rgba(0,0,0,0.7);
+    z-index: 200;
+    #popupHB{
+        position: absolute;
+        top: 20px;
+        right: 10px;
+        width: 55px;
+        height: auto;
+        transform:rotate(10deg);
+    }
+    #centerBox{
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        width: 70%;
+        max-width: 250px;
+        #popupImg1{
+            width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+
+        }
+        #popupImg2{
+            width: 25px;
+            height: auto;
+            display: block;
+            margin: 20px auto;
+
+        }
+        #popupImg3{
+            width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+
+
+        }
+    }
+}
+#upgraded{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: rgba(0,0,0,0.7);
+    z-index: 200;
+    #upgradedHB{
+        position: absolute;
+        top: 20px;
+        right: 10px;
+        width: 55px;
+        height: auto;
+        transform:rotate(10deg);
+    }
+    #centerBox2{
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-45%);
+        width: 80%;
+        max-width: 300px;
+        #popupImg4{
+            width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+
+        }
+        #popupImg5{
+            width:100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+        #popupImg6{
+            width: 25px;
+            height: auto;
+            display: block;
+            margin: 20px auto;
+        }
+
+    }
+}
+.ProfitAnima{
+    animation-name: myAnimaiton;
+    animation-duration: 10s;
+    animation-iteration-count:infinite;
+}
+#ProfitDom{
+    position: absolute;
+    left: 50%;
+    top: 100%;
+    opacity: 0;
+    transform: translate(-50%);
+    img{
+        width: 20px;
+        height: auto;
+        vertical-align: middle;
+    }
+    span{
+        font-size: 14px;
+        vertical-align: middle;
+        color: #fff;
+    }
+}
+@keyframes myAnimaiton {
+    0% {
+        top: 100%;
+        opacity: 0;
+    }
+    40%{
+        top: 50%;
+        opacity: 0;
+    }
+    50%{
+        top: 0;
+        opacity: 1;
+    }
+    60%{
+        top: 0;
+        opacity: 0;
+    }
+    100% {
+        top: 0;
+        opacity: 0;
+    }
+}
+#noIntegral{
+    position: absolute;
+    z-index: 200;
+    font-size: 20px;
+    color: #fff;
+    text-shadow: #555 1px 0 0,#555 0 1px 0,#555 -1px 0 0 ,#555 0 -1px 0;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background: rgba(0,0,0,0.5);
+    padding: 5px 15px;
+    width: fit-content;
+    border-radius: 5px;
+}
 </style>
